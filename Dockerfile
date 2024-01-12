cc95e4b0ca2d12f3ffba99f546016d4aca0c4a2e
yashukun <yashlolgreat@gmail.com>
2024-01-12 18:54:06 +0530

    final commit

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,14 @@
+# Base image is alpine with Go installed
 FROM golang:alpine
+
+# Set the working directory to the app directory
 WORKDIR /app
+
+# Copy all files from the current directory to the app directory within the container
 COPY . .
 
+# Install dependencies and build the application
 RUN go get && go build -o bin .
+
+# Set the entrypoint to the built application
 ENTRYPOINT ["/app/bin"]
--- main.go
+++ main.go
@@ -1,17 +1,23 @@
-package main
+package main // Declare the main package
 
 import (
-    "net/http"
+  "net/http"   // Import the net/http package for HTTP functionality
 
-    "github.com/gin-gonic/gin"
+  "github.com/gin-gonic/gin" // Import the Gin framework for web development
 )
-func main() {
-    router := gin.Default()
-    router.GET("/ping", func(c *gin.Context){
-        c.JSON(http.StatusOK, gin.H{
-          "message": "JAISHREERAM",
-        })
-    })
-    router.Run()
-}
 
+func main() {
+  // Create a new Gin router
+  router := gin.Default()
+
+  // Define a GET route for "/ping"
+  router.GET("/ping", func(c *gin.Context) {
+    // Send a JSON response with the message "JAISHREERAM" and a 200 OK status code
+    c.JSON(http.StatusOK, gin.H{
+      "message": "JAISHREERAM",
+    })
+  })
+
+  // Start the Gin server and listen on the default port (8080)
+  router.Run()
+}
